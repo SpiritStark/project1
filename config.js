68f45460ca360645349dfb6009182ec78cf8f4f1
SpiritStark <86712990+SpiritStark@users.noreply.github.com>
2022-11-08 09:45:47 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,13 +5,13 @@ const CONFIG = {
     btnIntro: 'hihi',
     title: 'Cá cắn câu biết đâu mà gỡ',
     desc: 'Chim vào lồng biết thuở nào ra',
-    btnYes: 'Vẫn cứ là thích anh <33',
+    btnYes: 'Vẫn cứ là tối mai đi ăn tối với anh nhé',
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại gặp phải anh nhỉ :D',
     btnReply: 'Gửi cho anh :)))',
-    reply: 'Bởi vì Anh còn cầm của em cái áo chống nắng',
+    reply: 'Bởi vì anh còn chưa đưa sách tiếng Trung cho em',
     mess: 'Anh biết mà 🥰',
-    messDesc: 'Cuối tuần này anh qua đón nhé công chúa.',
+    messDesc: 'Ngày mai anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/nam.nguyenha.3760/' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/nam.nguyenha.3760/' 
 }
